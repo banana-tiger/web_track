853105d13911b648753d0e28aa00dac8a4b95784
Anton Maliutin <amalutin13@gmail.com>
2020-03-16 11:38:30 +0300

    BeautifulSoup lesson

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from weather import weather_by_city
+from python_org_news import get_python_news
 
 app = Flask(__name__)
 
@@ -8,7 +9,8 @@ app = Flask(__name__)
 def index():
     page_title = 'Новости Python'
     weather = weather_by_city('Krasnodar,Russia')
-    return render_template('index.html', page_title=page_title, weather=weather)
+    news_list = get_python_news()
+    return render_template('index.html', page_title=page_title, weather=weather, news_list=news_list)
 
 
 if __name__ == '__main__':
