ab55003a5b80197d27def248d1f53770577012c8
Anton Maliutin <amalutin13@gmail.com>
2020-03-21 14:08:33 +0300

    создана БД, импорт новостей в бд, вывод на сайте

--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -1,5 +1,8 @@
+from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
+from webapp.model import db, News
+
 
 def get_html(url):
     try:
@@ -9,6 +12,7 @@ def get_html(url):
     except(requests.RequestException, ValueError):
         print('Сетевая ошибка')
         return False
+
 
 def get_python_news():
     html = get_html('https://www.python.org/blogs/')
@@ -20,10 +24,18 @@ def get_python_news():
             title = new.find('a').text
             url = new.find('a')['href']
             published = new.find('time').text
-            result_news.append({
-                'title': title,
-                'url': url,
-                'published': published,
-            })
-        return result_news
+            try:
+                published = datetime.strptime(published, '%Y-%m-%d')
+            except ValueError:
+                published = datetime.now()
+            save_news(title, url, published)
     return False
+
+
+def save_news(title, url, published):
+    news_exists = News.query.filter(News.url == url).count()
+    print(news_exists)
+    if not news_exists:
+        new_news = News(title=title, url=url, published=published)
+        db.session.add(new_news)
+        db.session.commit()
